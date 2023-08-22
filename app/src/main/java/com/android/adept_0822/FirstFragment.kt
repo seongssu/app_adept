1ae7e09334cb40659c707349325035a219dd2ff6
seongssu <loveforggong@gmail.com>
2023-08-23 06:50:19 +0900

    [FEAT] : ⚙️ fragment(완료, 주석)

--- app/src/main/java/com/android/adept_0822/FirstFragment.kt
+++ app/src/main/java/com/android/adept_0822/FirstFragment.kt
@@ -13,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [FirstFragement.newInstance] factory method to
+ * Use the [FirstFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class FirstFragement : Fragment() {
+class FirstFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -49,7 +49,7 @@ class FirstFragement : Fragment() {
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            FirstFragement().apply {
+            FirstFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
