f5e2954f293fb2b025647f48eb9324b22356d8bb
seongssu <loveforggong@gmail.com>
2023-08-22 14:31:36 +0900

    [PACK] : 🎁 5.1 viewBinding 예제

---
+++ app/src/main/java/com/android/adept_0822/veiwBindingActivity.kt
@@ -0,0 +1,11 @@
+package com.android.adept_0822
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class veiwBindingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
