b6756263028d344d85da0e116aa1a751495b28c2
seongssu <loveforggong@gmail.com>
2023-08-22 19:33:21 +0900

    [FEAT] : ⚙️ CustomItem(완료, 주석)

--- app/src/main/java/com/android/adept_0822/CustomItemAdapter.kt
+++ app/src/main/java/com/android/adept_0822/CustomItemAdapter.kt
@@ -8,7 +8,7 @@ import android.widget.BaseAdapter
 import android.widget.ImageView
 import android.widget.TextView
 
-class CustomItemAdapter(val mContext: Context, val mItems: MutableList<MyItem> ): BaseAdapter() {
+class CustomItemAdapter(val mContext: Context, val character: MutableList<MyItem> ): BaseAdapter() {
 
     override fun getCount(): Int {
         return character.size
@@ -21,7 +21,12 @@ class CustomItemAdapter(val mContext: Context, val mItems: MutableList<MyItem> )
     }
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
         var convertView = convertView
+        // convertView :재활용을 위해 이전에 표시한 뷰
+        // 이 변수가 null이 아닌경우 이전에 표시한 뷰를 재사용 할 수 있다.
         if(convertView == null) convertView = LayoutInflater.from(parent?.context).inflate(R.layout.item, parent, false)
+        //LayoutInflater.from(parent?.context) : 어댑터뷰(parent)의 context 사용해서  LayoutInflater의 객체를 생성한다.
+        //R.layout.item : 사용할 xml파일의 ID
+        //false : 부모 뷰에 즉시 첨부하지 않도록 설정
         val item : MyItem = character[position]
 
         val iconImageView = convertView?.findViewById<View>(R.id.iconItem) as ImageView
@@ -29,8 +34,11 @@ class CustomItemAdapter(val mContext: Context, val mItems: MutableList<MyItem> )
         val tv_age = convertView?.findViewById<View>(R.id.textItem2) as TextView
 
         iconImageView.setImageResource(item.aIcon)
+        //setImageResource : 이미지 뷰에 이미지를 설정하는 역할
+        //item.aIcon : 이미지 리소스와 리소스 ID (item : 데이터클래스, aIcon : item데이터 클래스에서 이미지아이콘을 나타내는 변수)
         tv_name.text = item.aName
-        tv_name.text = item.aAge
-    }
+        tv_age.text = item.aAge
 
+        return convertView
+    }
 }
